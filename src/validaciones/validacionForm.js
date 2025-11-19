9dc6c06b17bdcace9c5a409621b4fe911da9a80e
DMCHKN3 <ejimenezv2304@alumno.ipn.mx>
2025-11-19 15:16:58 -0600

    se arregla un bug que enviaba una alerta inscorrecta relacionada a la contraseña y se remueven campos de verificacion debido a que los datos ya no se solicitan

--- src/validaciones/validacionForm.js
+++ src/validaciones/validacionForm.js
@@ -1,15 +1,12 @@
 import { Alert } from "react-native";
-import { verifBoleta } from "../../BD/authService";
+import { verifBoleta } from "../../BD/authService"; //
 
-export const validarform = (user, contra, repcontra, nombre, apellidos, correo) => {
+export const validarform = (user, contra, repcontra, correo) => {
     const resUser = /^[0-9]{10}$/;
     const resContra = /^[A-Za-z0-9\-_.,"#%]{7,16}$/;
-    const resRepContra = /^[A-Za-z0-9\-_.,"#%]{7,16}$/;
-    const resNombre = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,100}$/;
-    const resApellidos = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]{2,100}$/;
     const resCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,100}$/;
 
-    if (!user || !contra || !nombre || !apellidos || !correo || !repcontra) {
+    if (!user || !contra || !correo || !repcontra) {
         Alert.alert('Error', 'Por favor complete todos los campos');
         return false;
     }
@@ -18,32 +15,22 @@ export const validarform = (user, contra, repcontra, nombre, apellidos, correo) 
         Alert.alert('Error', 'El usuario debe ser tu boleta de 10 dígitos');
         return false;
     }
-    if (!resContra.test(contra)) {
-        Alert.alert('Error', 'La contraseña solo puede contener letras, números y los símbolos -_.,”#%');
-        return false;
-    }
-    if (!resRepContra.test(repcontra)) {
-        Alert.alert('Error', 'Favor de introducir la misma contraseña')
-        return false;
-    }
-
-    if (!resNombre.test(nombre)) {
-        Alert.alert('Error', 'El nombre solo puede contener letras y espacios con un mínimo de 2 caracteres y máximo de 100');
-        return false;
-    }
-
-    if (!resApellidos.test(apellidos)) {
-        Alert.alert('Error', 'Los apellidos solo pueden contener letras y espacios con un mínimo de 2 caracteres y máximo de 100');
-        return false;
-    }
 
     if (!resCorreo.test(correo)) {
         Alert.alert('Error', 'El correo debe de tener un formato válido (ejemplo@dominio.com)');
         return false;
     }
+    
+    if (contra.length < 7 || contra.length > 16) {
+        Alert.alert('Error', 'La contraseña debe tener entre 7 y 16 caracteres');
+        return false;
+    } else if (!resContra.test(contra)) {
+            Alert.alert(' La contraseña debe incluir letras, números y símbolos -_.,”#% ');
+            return false;
+        }
 
     if (contra !== repcontra) {
-        Alert.alert('Error', 'Las contraseñas no coinciden');
+        Alert.alert('Error', 'Favor de introducir la misma contraseña')
         return false;
     }
 
